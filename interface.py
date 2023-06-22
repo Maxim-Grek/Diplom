bd8481e7638df833a1472e0fb3211b469fdd2ca8
Maxim-Grek <127651643+Maxim-Grek@users.noreply.github.com>
2023-06-22 21:00:13 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 from config import comunity_token, acces_token
-from core import VkTools
+from base import VkTools
 
 class BotInterface():
 
